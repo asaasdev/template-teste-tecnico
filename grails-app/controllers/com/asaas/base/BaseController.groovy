17ab464987b50553a6e05aec95fe15b409779478
Eduardo Pitz <pitz.eduardo@gmail.com>
2020-10-30 13:58:28 -0300

    Refatorando códgio

--- grails-app/controllers/com/asaas/base/BaseController.groovy
+++ grails-app/controllers/com/asaas/base/BaseController.groovy
@@ -1,7 +1,5 @@
 package com.asaas.base
 
-import com.asaas.enums.message.MessageType
-
 class BaseController {
 
     protected void buildResponse(Boolean success, String messageDescription, entity) {
@@ -17,21 +15,5 @@ class BaseController {
             Boolean useEntityError = (entity?.hasErrors() && entity.errors.allErrors[0].codes[0])
             if (useEntityError) flash.message = message(code: entity.errors.allErrors[0].codes[0] ?: entity.errors.allErrors[0].defaultMessage)
         }
-    }
-
-    protected buildAjaxResponse (Boolean success, String mainMessage, entity) {
-        def response = ['success': success]
-
-        MessageType type = success ? MessageType.SUCCESS : MessageType.ERROR
-
-        String subMessage = mainMessage
-
-        if (!subMessage && entity?.hasErrors() && entity.errors.allErrors[0].codes[0]) {
-            subMessage = message(code: entity.errors.allErrors[0].defaultMessage ?: entity.errors.allErrors[0].codes[0])
-        }
-
-        response.put('messageHtml', g.render(template: "/utils/message/message", model: ['type': type, 'message': mainMessage, 'entity': entity]))
-        response += [message: [type: type.toString().toLowerCase(), text: subMessage]]
-        return response
     }
 }
